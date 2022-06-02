fc6ab6f849d9b29389006d4b638e40f0fb4e8b70
Eduardo Binatti <edubinatti@gmail.com>
2022-06-02 14:55:36 -0300

    ajustes no server

--- projeto-rocketseat-impulse-server/src/server.ts
+++ projeto-rocketseat-impulse-server/src/server.ts
@@ -4,13 +4,11 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(cors(
-    {origin: 'http://localhost:3000'}
-));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('HTTP server is running on http://localhost:3333 !');
 });
 
